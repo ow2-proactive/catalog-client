d413699fe2dfceb8cb1510740407c4e5391d56bb
MaelAudren <mael.audren@activeeon.com>
2017-08-03 09:43:22 +0200

    remove size check because HEAD is not implemented in calendar

--- src/main/java/org/ow2/proactive/catalogclient/service/RemoteObjectService.java
+++ src/main/java/org/ow2/proactive/catalogclient/service/RemoteObjectService.java
@@ -25,10 +25,6 @@
  */
 package org.ow2.proactive.catalogclient.service;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.ow2.proactive.catalogclient.exception.EntityTooLargeException;
 import org.ow2.proactive.catalogclient.exception.FailedRequestException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
@@ -44,9 +40,6 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class RemoteObjectService {
 
-    //max length response equal 1 MB
-    private static final long MAX_LENGTH = 1000000;
-
     /**
      * Send a POST request on url returning a returnClass object
      * @param url is the url where the post request will be sent
@@ -55,23 +48,10 @@ public class RemoteObjectService {
      */
     public Object sendRequest(String url, Class returnClass) {
         try {
-            long resourceLength = new RestTemplate().headForHeaders(new URI(url)).getContentLength();
-            if (resourceLength > MAX_LENGTH) {
-                String message = "Get on url " + url + " failed because response size length " + resourceLength +
-                                 " is higher than max size " + MAX_LENGTH;
-                log.info(message);
-                throw new EntityTooLargeException(message);
-            }
-
             return new RestTemplate().getForObject(url, returnClass);
-
         } catch (RestClientException e) {
             String message = "Get request on the URL " + url + " failed. The server response was " +
                              e.getLocalizedMessage();
-            log.info(message, e);
-            throw new FailedRequestException(message, e);
-        } catch (URISyntaxException e) {
-            String message = "The url '" + url + "' is ill-formed.";
             log.info(message, e);
             throw new FailedRequestException(message, e);
         }
